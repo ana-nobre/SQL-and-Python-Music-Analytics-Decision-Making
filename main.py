7c22fe0c66d6664d024905eb83fed954c099ea83
Ana <anainez@icloud.com>
2025-09-25 00:18:01 +0200

    Add feature def extract_artist()

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from src import support_call_API as ap
 from src import data_manipulation as dm
 
-genre_list = ['rock', 'jazz', 'Pop']
+genre_list = ['rock', 'jazz', 'pop']
 for genre in genre_list:
     resultados, artist_list = ap.call(genre)
     dm.extract_artist(resultados, genre)
